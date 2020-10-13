f6cd029abd22768ec2f07099023cdebb64fba0a7
JamesMusyoka <jamesmu475@gmail.com>
2020-10-13 13:39:45 +0300

    connected our template with the view function.

--- news/views.py
+++ news/views.py
@@ -1,9 +1,10 @@
 from django.http  import HttpResponse,Http404
+from django.shortcuts import render
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 
 
